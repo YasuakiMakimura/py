5068d327d62986e4741c9e246863558fc983902e
Yasuaki Makimura <maki10310243@gmail.com>
2018-07-14 16:36:39 +0900

    generate_wの変更

--- Pytorch_NN/ESN.py
+++ Pytorch_NN/ESN.py
@@ -14,37 +14,33 @@ import function as OF
 class ESN(nn.Module):
 
     # Constructor
-    def __init__(self, input_dim, output_dim, feedback_dim, w=None, leak_rate=None,
-                 w_sparsity=None):
+    def __init__(self, input_dim, output_dim, feedback_dim, w=None, w_in=None, w_fb=None,
+                 leak_rate=None, w_sparsity=None, w_in_sparsity=None, w_fb_sparsity=None):
         super().__init__()
 
         # Params
         self.input_dim = input_dim
         self.output_dim = output_dim
         self.feedback_dim = feedback_dim
+        self.w_sparsity = w_sparsity
+        self.w_in_sparsity = w_sparsity
+        self.w_fb_sparsity = w_sparsity
         self.leak_rate = leak_rate
 
-        # Define connection
-        self.fc_in = nn.Linear(self.input_dim, self.output_dim) # fc : Full connection
-        self.fc_fb = nn.Linear(self.feedback_dim, self.output_dim)
-        self.fc_rec = nn.Linear(self.output_dim, self.output_dim)
+        # Define connect
+        self.fc_in = nn.Linear(self.input_dim, self.output_dim, bias=False)
+        self.fc_fb = nn.Linear(self.feedback_dim, self.output_dim, bias=False)
+        self.fc_rec = nn.Linear(self.output_dim, self.output_dim, bias=False)
 
         # Initialize W matrix
-        if w_sparsity is None:
-            if callable(w):
-                self.fc_rec.weight.data = w((self.output_dim, self.output_dim))
-            else:
-                self.fc_rec.weight.data.uniform_(-1.0, 1.0)
-
-        else:
-            self.w = np.random.choice([0.0, 1.0], (self.output_dim, self.output_dim),
-                                      p=[1.0 - w_sparsity, w_sparsity])
-            self.w = torch.FloatTensor(self.w)
-
-            if callable(w):
-                self.w[self.w == 1] = w((len(self.w[w == 1]), ))
-            else:
-                self.w[self.w == 1].uniform_(-1.0, 1.0, (len(self.w[w == 1]), ))
+        # todo apply()？を使ってLinearの時点でこの関数で重み値を初期化したほうが二度手間でない
+        # todo スパース結合を作ってくれるtorch.nn.Embeddingクラスがあるらしい
+        self.fc_in.weight.data = self.generate_w(w_in, self.w_in_sparsity,
+                                                 self.output_dim, self.input_dim)
+        self.fc_fb.weight.data = self.generate_w(w_fb, self.w_fb_sparsity,
+                                                 self.output_dim, self.feedback_dim)
+        self.fc_rec.weight.data = self.generate_w(w, self.w_sparsity,
+                                                  self.output_dim, self.output_dim)
 
         print(f'w: {self.fc_rec.weight.data}')
 
@@ -58,17 +54,48 @@ class ESN(nn.Module):
     # PUBLIC
     ##################################
 
+    # Forward
     def forward(self, u, x, old_y, fb):
-        self.u = self.leak_rate * self.u + (1 - self.leak_rate) * (
-                    self.fc_in(x) + self.fc_rec(old_y) + self.fc_fb(fb))
+        self.u = self.leak_rate * self.u + \
+                 (1 - self.leak_rate) * (self.fc_in(x) + self.fc_rec(old_y) + self.fc_fb(fb))
         y = F.tanh(u)
         return y
+
+    # Generate W matrix
+    # def generate_w(self, w, sparsity, *size):
+    #     if sparsity is None:
+    #         if callable(w):
+    #             return w(size)
+    #         else:
+    #             # r = np.random.uniform(-1.0, 1.0, size)
+    #             # return torch.FloatTensor(r)
+    #             return torch.zeros(size)
+    #     else:
+    #         s_w = np.random.choice([0.0, 1.0], size, p=[1.0 - sparsity, sparsity])
+    #         s_w = torch.FloatTensor(s_w)
+    #
+    #         if callable(w):
+    #             s_w[s_w == 1] = w((len(s_w[s_w == 1]),))
+    #         else:
+    #             s_w[s_w == 1].uniform_(-1.0, 1.0, (len(s_w[s_w == 1]),))
+    #
+    #         return s_w
+    # Generate W matrix
+    def generate_w(self, w, sparsity, *size):
+        if callable(w):
+            if sparsity is None:
+                return w(size)
+            else:
+                s_w = np.random.choice([0.0, 1.0], size, p=[1.0 - sparsity, sparsity])
+                s_w = torch.FloatTensor(s_w)
+                s_w[s_w == 1] = w((len(s_w[s_w == 1]),))
+        else:
+            return torch.zeros(size)
+
 
     def init_hidden(self):
 
         return torch.zeros(self.outptu_dim)
-
-
 
 
 if __name__ == '__main__':
